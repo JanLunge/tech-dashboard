fab69eb00185d43e6e4b8c490cb9ad4dbe3ee783
Jan Lunge <janlunge1993@gmail.com>
2022-04-29 00:08:29 +0200

    added basic dashboard

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex h-full">
+    <div class="w-1/5 grid grid-rows-4">
+      <div
+        class="bg-neutral-600 text-white flex flex-row items-center relative"
+      >
+        <p class="font-bold text-9xl text-center w-full">
+          18<span class="text-base font-normal">days</span>
+        </p>
+        <p class="text-right right-8 bottom-8 absolute flex">
+          <span>until the</span>
+          <img src="/src/assets/images/omr-logo.svg" alt="" class="h-8 mx-2" />
+          <span> festival</span>
+        </p>
+      </div>
+      <div
+        class="bg-neutral-300 text-black flex flex-row items-center relative"
+      >
+        <p class="font-bold text-9xl text-center w-full">
+          2<span class="text-base font-normal">days</span>
+        </p>
+        <p class="text-right right-8 bottom-8 absolute flex">
+          <span>until the MVP APP deadline</span>
+        </p>
+      </div>
+      <div class="row-span-2">shamin</div>
+    </div>
+    <div class="grid grid-rows-4">
+      <div>top</div>
+      <div class="row-span-3">bottom</div>
+    </div>
+  </div>
+</template>
